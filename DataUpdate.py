18002bea77b2f46535da4a73a5c591c7837e4a35
Nitro <nitro0@naver.com>
2020-04-12 02:22:38 +0900

    Updated a lot...

--- DataUpdate.py
+++ DataUpdate.py
@@ -7,7 +7,7 @@ urllib.request.urlretrieve(URL, 'data/data.json')
 URL = 'https://raw.githubusercontent.com/Nitro1231/COVID-19-Actions/master/LastUpdated/map.html'
 urllib.request.urlretrieve(URL, 'data/map.html')
 
-with open('index_en.html') as f:
+with open('index_template.html') as f:
     html = f.read()
 f.close
 
@@ -21,13 +21,19 @@ total_confirmed = str(jsonData['total_confirmed'])
 total_deaths = str(jsonData['total_deaths'])
 total_recovered = str(jsonData['total_recovered'])
 total_active = str(jsonData['total_active'])
+difference_confirmed = ''
+difference_deaths = ''
+difference_recovered = ''
+difference_active = ''
 mortality_rate = jsonData['mortality_rate']
 recovery_rate = jsonData['recovery_rate']
 
 mortality_rate = str(round(mortality_rate * 100, 2))
 recovery_rate =  str(round(recovery_rate * 100, 2))
 
-html = html.replace('total_confirmed', total_confirmed).replace('total_deaths', total_deaths).replace('total_recovered', total_recovered).replace('total_active', total_active).replace('mortality_rate', mortality_rate).replace('recovery_rate', recovery_rate)
+html = html.replace('total_confirmed', total_confirmed).replace('total_deaths', total_deaths).replace('total_recovered', total_recovered).replace('total_active', total_active)
+html = html.replace('difference_confirmed', difference_confirmed).replace('difference_deaths', difference_deaths).replace('difference_recovered', difference_recovered).replace('difference_active', difference_active)
+html = html.replace('mortality_rate', mortality_rate).replace('recovery_rate', recovery_rate)
 
 with open('index.html', 'w') as f:
     f.write(html)
